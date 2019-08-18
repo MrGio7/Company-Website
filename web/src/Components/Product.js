e0fac3072e26581bdfbf4e208b4bc7499f6b9dcb
MrGio7 <lashagiorgi1233@gmail.com>
2019-08-18 11:55:16 +0400

    added bootstrap cards

--- web/src/Components/Product.js
+++ web/src/Components/Product.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Card, Button } from "react-bootstrap";
 
 import "../style/product.scss";
 
@@ -11,21 +12,25 @@ const Product = () => {
     {
       name: "myne",
       description: "this is very good product",
+      img: 'https://i1217.photobucket.com/albums/dd388/jnelson9r/J%20Nelson%20Real%20Estate/JNELSONREALESTATE5.jpg',
       price: "80$"
     },
     {
       name: "nya",
       description: "this is not  product",
+      img: 'https://i718.photobucket.com/albums/ww187/talisson_01/House_of____by_dechobek.jpg',
       price: "0$"
     },
     {
       name: "jimi",
       description: "are u kiddin me",
+      img: 'https://i865.photobucket.com/albums/ab218/zola_wagner/Backofhouse.jpg',
       price: "2000$"
     },
     {
       name: "gulie",
       description: "god damn it",
+      img: 'https://i1217.photobucket.com/albums/dd388/jnelson9r/J%20Nelson%20Real%20Estate/JNELSONREALESTATE4.jpg',
       price: "2$"
     }
   ];
@@ -33,11 +38,15 @@ const Product = () => {
   return (
     <div className="productPage">
       {customList.map((item, index) => (
-        <div className="product" key={index}>
-          <h1>Name: {item.name}</h1>
-          <p>Description: {item.description}</p>
-          <h2>Price: {item.price}</h2>
-        </div>
+        <Card style={{ width: "18rem" }} key={index}>
+          <Card.Img variant="top" src={item.img} />
+          <Card.Body>
+            <Card.Title>{item.name}</Card.Title>
+            <Card.Text>{item.description}</Card.Text>
+            <Card.Text>Price: {item.price}</Card.Text>
+            <Button variant="primary">View Detiled</Button>
+          </Card.Body>
+        </Card>
       ))}
     </div>
   );
