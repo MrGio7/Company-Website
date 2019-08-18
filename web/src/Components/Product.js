f2a30c6847106e110e1f5d0d5ec9532025068683
MrGio7 <lashagiorgi1233@gmail.com>
2019-08-18 11:03:17 +0400

    imported custom data to product page

--- web/src/Components/Product.js
+++ web/src/Components/Product.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Product = () => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("this is very good product");
+  const [price, setPrice] = useState("80$");
+
+  const customList = [
+    {
+      name: "myne",
+      description: "this is very good product",
+      price: "80$"
+    },
+    {
+      name: "nya",
+      description: "this is not  product",
+      price: "0$"
+    },
+    {
+      name: "jimi",
+      description: "are u kiddin me",
+      price: "2000$"
+    }
+  ];
+
   return (
     <div>
-      <h1>Product Page</h1>
+      {customList.map((item, index) => (
+        <div key={index}>
+          <h1>Name: {item.name}</h1>
+          <p>Description: {item.description}</p>
+          <h2>Price: {item.price}</h2>
+        </div>
+      ))}
     </div>
   );
 };
