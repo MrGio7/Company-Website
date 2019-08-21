9c9357c73a5790639773ee38f73bb864a5c1f369
MrGio7 <lashagiorgi1233@gmail.com>
2019-08-21 11:40:35 +0400

    added product page styling

--- web/src/Components/Product.js
+++ web/src/Components/Product.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Alert } from "react-bootstrap";
 
 import "../style/product.scss";
 
@@ -21,18 +21,32 @@ const Product = () => {
   });
 
   return (
-    <div className="productPage">
-      {data.productList.map((item, index) => (
-        <Card style={{ width: "18rem" }} key={index}>
-          <Card.Img variant="top" src={item.img} />
-          <Card.Body>
-            <Card.Title>{item.name}</Card.Title>
-            <Card.Text>{item.description}</Card.Text>
-            <Card.Text>Price: {item.price}</Card.Text>
-            <Button variant="primary">View Detiled</Button>
-          </Card.Body>
-        </Card>
-      ))}
+    <div>
+      {!localStorage.token ? (
+        <div className="alert">
+          <Alert variant="danger">
+            <Alert.Heading>Hey, nice to see you</Alert.Heading>
+            <p>Sorry But you neet to login first.</p>
+            <hr />
+            <p className="mb-0">
+              If you don`t have account, please register first.
+            </p>
+          </Alert>
+        </div>
+      ) : (
+        <div className="productPage">
+          {data.productList.map((item, index) => (
+            <Card style={{ width: "18rem" }} key={index}>
+              <Card.Img variant="top" src={item.img} />
+              <Card.Body>
+                <Card.Title>{item.name}</Card.Title>
+                <Card.Text>Price: {item.price}</Card.Text>
+                <Button variant="primary">View Detiled</Button>
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
