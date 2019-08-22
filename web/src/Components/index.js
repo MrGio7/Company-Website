4beb37f483bde731f0a6dfb557cf443faec901cc
MrGio7 <lashagiorgi1233@gmail.com>
2019-08-22 20:14:51 +0400

    moved products js to one folder

--- web/src/Components/index.js
+++ web/src/Components/index.js
@@ -1,8 +1,8 @@
 import Home from "./Home";
 import Navigation from "./Navigation";
 import About from "./About";
-import Product from "./Product";
-import AddProduct from "./Add-Product";
-import DetiledProd from "./Detiled-Product";
+import Product from "./Products/Product";
+import AddProduct from "./Products/Add-Product";
+import DetiledProd from "./Products/Detiled-Product";
 
 export { Home, Navigation, About, Product, AddProduct, DetiledProd };
