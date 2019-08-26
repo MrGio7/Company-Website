ed9e465fedaeb125cdac0f5347f1df64c5e0ab1d
MrGio7 <lashagiorgi1233@gmail.com>
2019-08-26 14:03:27 +0400

    added comments get

--- server/api/comments/comments.js
+++ server/api/comments/comments.js
@@ -4,8 +4,17 @@ const knexConfig = require("../../knexfile.js");
 
 const db = knex(knexConfig.development);
 
-router.get("/")
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
 
-
+  db("comments")
+    .where({ id: id_product })
+    .then(comments => {
+      res.status(201).json(comments);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
 export default router;
